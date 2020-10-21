61ef84a32d8395cabab6eea4d5f187a678af6e88
Carlos Roth <>
2020-10-21 14:13:38 +0200

    no message

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { CartService } from "../../Services/cart.service";
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../Services/cart.service';
 
 @Component({
-	selector: "app-cart",
-	templateUrl: "./cart.component.html",
-	styleUrls: ["./cart.component.css"],
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-	items;
-	total = 0;
+  items;
+  total = 0;
 
-	constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService) {}
 
-	clearItem(product) {
-		this.cartService.clearItem(product);
-	}
-	clearCart() {
-		this.items = this.cartService.clearCart();
-	}
+  clearItem(product) {
+    this.cartService.clearItem(product);
+    this.total -= product.price;
+  }
+  clearCart() {
+    this.items = this.cartService.clearCart();
+  }
 
-	ngOnInit(): void {
-		this.items = this.cartService.getItems();
-		this.items.forEach((item) => {
-			this.total += item.price;
-		});
-	}
+  ngOnInit(): void {
+    this.items = this.cartService.getItems();
+    this.items.forEach((item) => {
+      this.total += item.price;
+    });
+  }
 }
--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -1,30 +1,30 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable({
-	providedIn: "root",
+  providedIn: 'root',
 })
 export class CartService {
-	items = [];
+  items = [];
 
-	addToCart(product) {
-		this.items.push(product);
-	}
+  addToCart(product) {
+    this.items.push(product);
+  }
 
-	getItems() {
-		return this.items;
-	}
+  getItems() {
+    return this.items;
+  }
 
-	clearCart() {
-		this.items = [];
-		return this.items;
-	}
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 
-	clearItem(product) {
-		const index: number = this.items.indexOf(product);
-		if (index !== -1) {
-			this.items.splice(index, 1);
-		}
-	}
+  clearItem(product) {
+    const index: number = this.items.indexOf(product);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
 
-	constructor() {}
+  constructor() {}
 }
